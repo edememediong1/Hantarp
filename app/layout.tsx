87a655c475493c47870cdf24e30536db1e2a81a0
Emediong Bassey Edem <edememediong1@gmail.com>
2025-05-06 09:38:47 +0100

    feature: added routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Navbar } from "@/components/Navbar";
+import { Montserrat } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  display: 'swap',
+})
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,8 +24,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={montserrat.className}
       >
+        <Navbar />
         {children}
       </body>
     </html>
---
+++ components/Home/Contact.tsx
@@ -0,0 +1,13 @@
+import Forms from './Forms'
+import Map from './Map'
+
+function Contact() {
+  return (
+    <div>
+        <Map />
+        <Forms />
+    </div>
+  )
+}
+
+export default Contact
